dfea555829ee0b5b8bb5f4822a3e9aaf17ded55f
Mohame-Ragab <moha.hamdy1998@gmail.com>
2023-08-03 16:34:17 +0300

    custom appBar and Notes item

---
+++ lib/views/widgets/custom_search_icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomSearhIcon extends StatelessWidget {
+  const CustomSearhIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(.05),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Icon(
+          Icons.search,
+          size: 28,
+        ),
+      ),
+    );
+  }
+}
