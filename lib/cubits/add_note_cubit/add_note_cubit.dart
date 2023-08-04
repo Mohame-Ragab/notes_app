89e13e1fd0b22c10d065800c919e1d429a850188
Mohame-Ragab <moha.hamdy1998@gmail.com>
2023-08-04 20:35:43 +0300

    refactor multi bloc provider

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/constants.dart';
@@ -16,9 +16,9 @@ class AddNoteCubit extends Cubit<AddNoteState> {
 
       await notesBox.add(note);
       emit(AddNoteSuccess());
-    } on Exception catch (e) {
-      // emit(AddNoteFailure(e.toString()));
-      AddNoteFailure(e.toString());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+      // AddNoteFailure(e.toString());
     }
   }
 }
